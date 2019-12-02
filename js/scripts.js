097ee5ea834943719f914f61fb5bb24eee7ac692
Flor Nazari <florm82@gmail.com>
2019-12-01 16:59:55 -0800

    add payment select to one of the function

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var person1Input = $("input#person1").val();
-    var paymentInput = $("input#payment").val();
+    var paymentSelect = $("select#payment").val();
     var addressInput= $("input#address").val();
 
 
     $(".person1").text(person1Input);
-    $(".payment").text(paymentInput);
+    $(".payment").text(paymentSelect);
     $(".address").text(addressInput);
 
 
