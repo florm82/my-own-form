86a251d759beef39d9a424d71136ea73f9cbe264
Flor Nazari <florm82@gmail.com>
2019-12-02 23:31:30 -0800

    add new functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,16 @@ $(document).ready(function() {
     var person1Input = $("input#person1").val();
     var paymentSelect = $("select#payment").val();
     var addressInput= $("input#address").val();
-
+    var shipmentInput = $("input:radio[name=shipment]:checked").val();
+    var productInput = $("input:radio[name=product]:checked").val();
+    var ddateInput= $("input#ddate").val();
 
     $(".person1").text(person1Input);
     $(".payment").text(paymentSelect);
     $(".address").text(addressInput);
-
+    $(".shipment").text(shipmentInput);
+    $(".product").text(productInput);
+    $(".ddate").text(ddateInput);
 
     $("#confirmation receipt").show();
 
